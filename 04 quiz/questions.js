23f01bb1ca896001cbde59d02964408bb7c84bff
Diako Amir <sabina.diako@gmail.com>
2019-11-28 22:37:32 +0300

    initiate commit

---
+++ 04 quiz/questions.js
@@ -0,0 +1,5 @@
+var arrQuestion = [
+                {q:"1+1 = ?",A:"15",B:"6",C:"2",D:"1",right:'C',answer:null},
+                {q:"sin(PI)",A:"1",B:"0",C:"0.5",D:"200",right:'B',answer:null},
+                {q:"Is 7 Prime?",A:"YES",B:"NO",C:"Neither",D:"-",right:'A',answer:null}
+            ];
